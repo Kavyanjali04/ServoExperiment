8c3c312cd61bdb13fb8baaf3afe1fffe89a39a23
Kavyanjali04 <kavyanjali.b04@gmail.com>
2024-10-22 22:13:21 +0530

    Add files via upload

---
+++ ServoExperiment.ino
@@ -0,0 +1,20 @@
+#include<Servo.h>
+Servo myservo;
+#include<Servo.h>
+int val, ServoPin=9, angle=0;
+void setup(){
+  Serial.begin(9600);
+  myservo.attach(ServoPin);
+}
+void loop(){
+myservo.write(0);
+delay(100);
+myservo.write(60);
+delay(100);
+myservo.write(90);
+delay(100);
+myservo.write(120);
+delay(100);
+myservo.write(180);
+delay(100);
+}
